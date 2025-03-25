4fea8d3ca39fd538dc1f324ab3a50fab93b47815
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-03-25 09:31:48 -0300

    Rotas no Django e o arquivo urls.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+"""
+Nota do aluno:
+Após criar a aplicação o primeiro passo é criar a pasta templates na raiz da aplicação. 
+Dentro dessa pasta para em seguida criar os os métodos views que serão responsáveis 
+por renderizar as páginas HTML do arquivo chamado:
+    index.html
+    contato.html
+"""
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def contato(request):
+    return render(request, "contato.html")
